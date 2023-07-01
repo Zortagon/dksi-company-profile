bef195aa455f684058d1318a7c6a3a3714be7dc5
Aldian Fernanda <zortagundul@gmail.com>
2023-07-02 00:44:09 +0700

    Menyesuaikan CSS dan membuat core JS

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper-partners", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});

6458d73dc8df77d5ff9ed4807961a0b834d63a30
Aldian Fernanda <zortagundul@gmail.com>
2023-07-02 11:48:12 +0700

    Mengatur ulang struktur section partner dan menambahkan assets logo partner

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,18 @@
 var swiper = new Swiper(".swiper-partners", {
     loop: true,
+    spaceBetween: 75,
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
     autoplay: {
         delay: 2500,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
+        pauseOnMouseEnter: true
     },
+    breakpoints: {
+        640: {
+            spaceBetween: 0
+        }
+    }
 });

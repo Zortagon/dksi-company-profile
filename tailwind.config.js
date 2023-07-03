0ea0214e544a9ac4cea620e954278a8b13d9d61f
Aldian Fernanda <zortagundul@gmail.com>
2023-07-04 03:24:00 +0700

    Memindahkan colors ke bagian extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'darken-blue': '#0B1F2A',
+                'darken-gold': '#AA823C',
+                'medium-white': '#FBFBFB',
+                'light-gray': '#F2F2F2',
+                'light-gold': '#CA9D42',
+                'white': '#FFFFFF',
+                'cream': '#F4EBD9',
+            }
+        },
         fontFamily: {
             'roboto': ['Roboto Mono', 'ui-monospace']
         },
@@ -14,16 +24,7 @@ module.exports = {
                 lg: '4rem',
                 xl: '5rem',
             }
-        },
-        colors: {
-            'darken-blue': '#0B1F2A',
-            'darken-gold': '#AA823C',
-            'medium-white': '#FBFBFB',
-            'light-gray': '#F2F2F2',
-            'light-gold': '#CA9D42',
-            'white': '#FFFFFF',
-            'cream': '#F4EBD9',
-        }
+        }   
     },
     plugins: [],
 }

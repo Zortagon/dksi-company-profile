1b1b8a011394ac8097add99994e4d6f6c3d840fa
Aldian Fernanda <zortagundul@gmail.com>
2023-07-21 13:18:34 +0700

    Menambah extend 'colors'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./public/**/*.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'white-background': '#f9fafe'
+            }
+        },
     },
     plugins: [],
 }

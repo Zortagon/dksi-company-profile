279e01be6ea9a3b91bcff04f9d7aff1054592dd6
Aldian Fernanda <zortagundul@gmail.com>
2023-07-23 16:45:16 +0700

    Memeprbarui code untuk tombol menu responsive navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,27 @@
 "use strict";
 
 $(document).ready(function () {
+    const navMain = $('#navbar-main');
+    const navMenu = $('#navbar-menu');
+    const navItem = $('.nav-link, .btn-contact');
+
+    function closeNavBar() {
+        navMenu.addClass('hidden');
+    }
+
     $('#menu-button').on('click', function (e) {
         e.stopPropagation();
         $('#navbar-menu').toggleClass('hidden');
     });
-    $('#navbar-menu').on('click', function (e) {
+
+    navMain.on('click', function (e) {
         e.stopPropagation();
     });
+    navMenu.on('click', function (e) {
+        e.stopPropagation();
+    });
+    navItem.on('click', closeNavBar)
 
     // Hide navbar sub-menu on click outside.
-    $(this).on('click', function () {
-        $('#navbar-menu').addClass('hidden');
-    });
+    $(this).on('click', closeNavBar);
 });

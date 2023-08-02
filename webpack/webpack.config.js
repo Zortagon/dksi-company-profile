b33ad8d9b302c00ac2e3574c71a8baf0f5fe3de1
Aldian Fernanda <zortagundul@gmail.com>
2023-08-02 22:36:06 +0700

    Install module babel-loader dan css-loader, postcss loader untuk setup postcss

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,39 @@
-const platformPath = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
     mode: 'production',
     entry: './src/js/index.js',
     output: {
-        path: platformPath.resolve(__dirname, '../public'),
+        path: path.resolve(__dirname, '../public'),
         clean: true,
-        filename: 'main.[contenthash].js',
+        filename: 'js/main.[contenthash].js',
+        environment: {
+            arrowFunction: false,
+        },
+    },
+    resolve: {
+        alias: {
+            jquery: 'jquery/dist/jquery.slim.js',
+        },
+    },
+    plugins: [
+        new HTMLWebpackPlugin({ template: 'src/index.html' }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    },
+                },
+            },
+        ],
     },
 }
